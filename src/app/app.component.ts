ceba9205af3df62fc660eef49fc497a6536c6c57
HichamGsb <hichamgsb.pro@gmail.com>
2023-10-03 07:22:56 +0200

    Setting and Patching Form Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,23 @@ export class AppComponent {
   @ViewChild('f') signupForm : NgForm;
   defaultQuestion = "pet";
   answer = '';
-  genders = ['male', 'female']; 
+  genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
